5f04bac0ce15b701d7ffd34c3b127361d88564a1
Ankit Das <ankitsonu240@gmail.com>
2024-08-03 11:48:32 +0530

    Regular Expression Challenge is Done

--- regEx.js
+++ regEx.js
@@ -1,3 +1,5 @@
+// Basic Regular Expressions Activity - 1
+
 function matchWord(word) {
     const pattern = /^(?=.*J)(?=.*a)(?=.*v)(?=.*S)(?=.*c)(?=.*r)(?=.*i)(?=.*p)(?=.*t).*$/i;
     if(pattern.test(word)) {
@@ -7,12 +9,14 @@ function matchWord(word) {
     }
 }
 
-// console.log(matchWord("Javascript"));
+console.log(matchWord("Javascript"));
 
 const pattern2 = /\d/g
 const word2 = "JavaScript is a very 345d language";
+
 console.log(word2.match(pattern2));
 
+// character classes and quantifiers Activity - 2
 
 function checkCapital(word, type){
     if(type === "firstWord"){
@@ -34,3 +38,67 @@ function matchNumberSequence(word) {
 }
 
 matchNumberSequence("JavaScript is a very 345d  1261 language")
+
+// gouping and capturing Activity - 3
+
+function extractCodes(str){
+    const regex = /\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})/;
+    
+    const match = str.match(regex);
+    if (match) {
+        const areaCode = match[1];
+        const centralOfficeCode = match[2];
+        const lineNumber = match[3];
+    
+        console.log(`Area Code: ${areaCode}`);
+        console.log(`Central Office Code: ${centralOfficeCode}`);
+        console.log(`Line Number: ${lineNumber}`);
+    } else {
+        console.log("No match found.");
+    }
+}
+
+extractCodes("(123) 456-7890");
+
+function extractDomain(str){
+    const emailPattern = /([^@]+)@([^@]+)/;
+    
+    const match = str.match(emailPattern);
+    if (match) {
+        const username = match[1];
+        const domain = match[2];
+    
+        console.log(`Username: ${username}`);
+        console.log(`Domain: ${domain}`);
+    } else {
+        console.log("No match found.");
+    }
+}
+
+extractDomain("example@domain.com");
+
+// Assertions and Boundaries Activity - 4
+
+const startWord = /^\b\w+\b/;
+const endWord = /\b\w+\b$/
+
+console.log("dog is a beautiful animal".match(endWord));
+console.log("dog is a beautiful animal".match(startWord));
+
+// Pratical Applications Activity - 5
+
+function validatePassword(password) {
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+    const isValid = regex.test(password);
+    console.log(`Password "${password}" is ${isValid ? 'valid' : 'invalid'}.`);
+}
+
+validatePassword("Passw34#sa")
+
+function validateURL(url) {
+    const regex = /^(https?:\/\/)?((([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,})|localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\[?[a-fA-F0-9:]+\]?)?(:\d{1,5})?(\/[a-zA-Z]*)*$/;
+    const isValid = regex.test(url);
+    console.log(`URL "${url}" is ${isValid ? 'valid' : 'invalid'}.`);
+}
+
+validateURL("http://localhost:8080/api");
